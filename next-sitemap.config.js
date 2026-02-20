b5b2f7e59ff28c523bb71119458a8c548a20e896
Caleb Luebbering <calube4042@gmail.com>
2026-02-19 23:15:10 -0600

    added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://calebluebbering.com', // replace with your real domain
+  generateRobotsTxt: true,           // (optional) generates robots.txt
+  priority: 0.8,                     // optional
+  sitemapSize: 5000,
+};
